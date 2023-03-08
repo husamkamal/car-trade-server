2a33a1441685e4c4ef9a40d3756140943ba07258
husamkamal <husamkmal94@gmail.com>
2023-03-08 15:08:56 +0200

    cookies 11154ss5

--- src/app.ts
+++ src/app.ts
@@ -18,23 +18,17 @@ dotenv.config();
 const app: Express = express();
 
 app.set('port', process.env.PORT || 4000);
-const corsOptions = {
-  origin: true,
-  // credentials: true,
-};
-app.use(cors(corsOptions));
-// app.options('*', cors());
-// // eslint-disable-next-line func-names
-// const allowCrossDomain = function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// };
+app.use(cors());
+app.use((req, res, next) => {
+  // Enabling CORS
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
+  next();
+});
 app.use([compression(),
   express.json(),
   cookieParser(),
-  // allowCrossDomain,
   express.urlencoded({ extended: false })]);
 
 if (process.env.NODE_ENV === 'development') {
